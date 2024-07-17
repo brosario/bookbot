cc73f93e2ccbcc784235264d79a4bc5241f5cfce
brosario <brianrosariovargas@gmail.com>
2024-07-16 23:23:24 -0400

    file report function now returns alphabet count of file. still need to format output

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 
+
+def dict_items_to_list(char_dict):
+    char_dict_keys = list(char_dict.keys())
+    char_dict_values = list(char_dict.values())
+
+    return char_dict_keys, char_dict_values
+
+def file_report(char_count):
+    char_dict_keys, char_dict_values = dict_items_to_list(char_count)
+    print(char_dict_keys)
+    print(char_dict_values)
+
+    alpha_count_dict = {}
+
+    for i in range(len(char_dict_keys)):
+        if char_dict_keys[i].isalpha():
+            alpha_count_dict[char_dict_keys[i]] = char_dict_values[i]
+
+    return alpha_count_dict
+
+
 def character_count(file):
     char_dict = {}
 
@@ -18,8 +39,11 @@ def word_count(file):
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-        print(word_count(file_contents))
-        print(character_count(file_contents))
+        #print(file_contents)
+        #print(word_count(file_contents))
+        char_count = character_count(file_contents)
+        print(file_report(char_count))
+
+
 
 main()
